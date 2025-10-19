e962531b2c264a64f1ddfcd6a8823aee0cceae9d
ChengDian9473 <adamcheng.gl@gmail.com>
2025-10-19 09:07:29 +0800

    Basic Move

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 namespace SoundTrack{
     public class GameManager : MonoBehaviour
     {
         public static GameManager Instance { get; private set; }
+
+        private bool playing;
 
         private void Awake()
         {
@@ -19,8 +22,41 @@ namespace SoundTrack{
 
         private void Start()
         {
+            playing = false;
             SceneManager.LoadScene("MainMenu");
             Info.Instance.GameInit();
+        }
+
+        private void Update(){
+            if(playing){
+                if(Keyboard.current.wKey.wasPressedThisFrame)
+                    Player.Instance.move(Vector3Int.up);
+                if(Keyboard.current.sKey.wasPressedThisFrame)
+                    Player.Instance.move(Vector3Int.down);
+                if(Keyboard.current.aKey.wasPressedThisFrame)
+                    Player.Instance.move(Vector3Int.left);
+                if(Keyboard.current.dKey.wasPressedThisFrame)
+                    Player.Instance.move(Vector3Int.right);
+                if (Mouse.current.rightButton.wasReleasedThisFrame){
+                    while(Player.Instance.Track.Count > 0){
+                        Destroy(Player.Instance.Track[0]);
+                        Player.Instance.Track.RemoveAt(0);
+                    }
+                }
+                if(Keyboard.current.eKey.wasPressedThisFrame && Player.Instance.Track.Count == 4){
+                    Debug.Log("Skill");
+                    while(Player.Instance.Track.Count > 0){
+                        Destroy(Player.Instance.Track[0]);
+                        Player.Instance.Track.RemoveAt(0);
+                    }
+                }
+            }
+        }
+        public void GameStart(){
+            playing = true;
+        }
+        public void GameEnd(){
+            playing = false;
         }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+namespace SoundTrack{
+    public class Player : MonoBehaviour
+    {   
+        public static Player Instance { get; private set; }
+
+        [Header("References")]
+        public Tilemap groundTilemap;
+        public TileBase[] allowedTiles;
+
+        public GameObject TrackPrefab;
+        
+        public List<GameObject> Track;
+
+        Vector3Int dir;
+        Vector3Int currentCell;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start(){
+
+            List<GameObject> Track = new List<GameObject>();
+
+            transform.position = new Vector3Int(0, 0, 0);
+            currentCell = groundTilemap.WorldToCell(transform.position);
+            transform.position = groundTilemap.GetCellCenterWorld(currentCell);
+            GameManager.Instance.GameStart();
+        }
+        
+        public void move(Vector3Int dir){
+            Vector3Int target = currentCell + dir;
+            Debug.Log(target);
+            if(IsWalkable(target)){
+                transform.Translate(dir);
+                if(Mouse.current.rightButton.isPressed){
+                    if(Track.Count < 4){
+                        Track.Add(Instantiate(TrackPrefab));
+                    }
+                    for(int i = Track.Count - 1; i > 0 ; i--){
+                        Track[i].transform.position = Track[i-1].transform.position;
+                        Track[i].transform.localScale = Track[i-1].transform.localScale * 0.8f;
+                        // Track[i].GetComponent<SpriteRenderer>.sortingOrder
+                    }
+                    Track[0].transform.position = groundTilemap.GetCellCenterWorld(currentCell);
+                }
+                currentCell = currentCell + dir;
+            }
+        }
+        bool IsWalkable(Vector3Int cell)
+        {
+            if (!groundTilemap.HasTile(cell)) return false;
+            TileBase t = groundTilemap.GetTile(cell);
+            foreach (var a in allowedTiles)
+                if (t == a) return true;
+            return false;
+        }
+    }
+}
