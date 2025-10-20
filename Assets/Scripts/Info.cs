f5da17ff6a86229c497d8408e0eb26022f7d7636
ChengDian9473 <adamcheng.gl@gmail.com>
2025-10-21 04:14:09 +0800

    Fix Build Bug

--- Assets/Scripts/Info.cs
+++ Assets/Scripts/Info.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
-using UnityEditor.Build;
 
 namespace SoundTrack{
     public class Info : MonoBehaviour
